cd2c7fa01f55e77683b392a037a70f06556b669d
Bruno Bonamin <bruno@bonamin.org>
2015-12-17 18:00:09 -0300

    Return cdata in encoded json

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'bundler/setup'
 
 require 'sinatra'
+require 'sinatra/json'
+
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 get '/docking_stations' do
   doc = Nokogiri::HTML(open('http://www.mibicitubici.gob.ar/estacione.aspx'))
-  doc.at('//*[@id="FormMaster"]/script[1]/text()').content
+  cdata = doc.at('//*[@id="FormMaster"]/script[1]/text()').content
+  json parsed(cdata)
+end
+
+def parsed(cdata)
+  contents = cdata.scan(/\[\[.+\]\];/)[0]
+  instance_eval(contents)
 end
