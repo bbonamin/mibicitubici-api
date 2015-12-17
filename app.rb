70cc7bb26bd6fa0eb03f55e3164b88d2ea34ca05
Bruno Bonamin <bruno@bonamin.org>
2015-12-17 18:31:19 -0300

    Convert matrix to array of objects with descriptive keys

--- app.rb
+++ app.rb
@@ -6,15 +6,30 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
+DockingStation = Struct.new(:address,
+                            :longitude,
+                            :latitude,
+                            :a,
+                            :b,
+                            :c,
+                            :d,
+                            :available_bikes,
+                            :anchor,
+                            :usage_status,
+                            :icon,
+                            :id,
+                            :station_name,
+                            :station_status)
+
 get '/docking_stations' do
   doc = Nokogiri::HTML(open('http://www.mibicitubici.gob.ar/estacione.aspx'), nil, 'utf-8')
   cdata = doc.at('//*[@id="FormMaster"]/script[1]/text()').content
 
   content_type 'application/json; charset=utf-8'
-  parsed(cdata).to_json
+  { data: parsed(cdata) }.to_json
 end
 
 def parsed(cdata)
-  contents = cdata.scan(/\[\[.+\]\];/)[0]
-  instance_eval(contents)
+  contents = instance_eval(cdata.scan(/\[\[.+\]\];/)[0])
+  contents.map{ |s| DockingStation.new(*s).to_h }
 end
